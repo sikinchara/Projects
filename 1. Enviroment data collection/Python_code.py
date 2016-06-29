fc58231a58b3803407b0a8f0916856b2c1e33aea
Zvonimir <z.pervan65@gmail.com>
2016-06-29 19:32:18 +0200

    Update the data enviroment code with a logistic regression classifier

--- 1. Enviroment data collection/Python_code.py
+++ 1. Enviroment data collection/Python_code.py
@@ -9,9 +9,9 @@ predikcijski model pomocu linearne i logisticke regresije.
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import linear_model
-#from sklearn.neural_network import BernoulliRBM as BNN
+import sklearn.cross_validation as cv
+from sklearn import metrics
 import numpy as np
-
 
 #Citanje podataka iz csv dokumenta
 df = pd.read_csv('soba.csv')
@@ -26,6 +26,7 @@ print ('Skup podataka se sastoji od {} elemenata\n'.format(len(df)))
 print df.describe()
 print('\n')
 #Standardna devijacija podataka (prosjecno odstupanje od prosjeka i to u apsolutnom iznosu)
+print ("Standardna devijacija podataka:")
 print df.std()
 print('\n')
 #Ispis prva tri elementa
@@ -34,7 +35,7 @@ print('\n')
 #Ispis zadnja tri elementa
 print "Ispis zadnja tri reda:\n",df.tail(3)
 #Ukupni pregled podataka pomocu histograma
-df.hist()
+df.hist(figsize=(8,8))
 
 #Pojedinacni histogrami
 vlaga = df['Vlaga']
@@ -74,55 +75,48 @@ plt.show()
 
 #Moramo ga oblikovat kao 1-d polje
 co2ts = co2.reshape([1,458])
+temperaturats = temperatura.reshape([1,458])
+vlagats = vlaga.reshape([1,458])
 vrijemets = pd.date_range('2016-03-02 21:15:00', periods=len(co2),freq='15T')
 #Pomocu naredbe ravel() ga pretvaramo u 1-d niz (array)
 ts_co2 = pd.Series(data=co2ts.ravel(), index = vrijemets)
+ts_temperatura = pd.Series(data=temperaturats.ravel(), index = vrijemets)
+ts_vlaga = pd.Series(data=vlagats.ravel(), index = vrijemets)
 plt.figure()
 plt.title('Kolicina CO2 od 03.02. do 07.02.')
 ts_co2.plot(grid=True)
-
+plt.figure()
+plt.title('Temperatura od 03.02. do 07.02.')
+ts_temperatura.plot(grid=True)
+plt.figure()
+plt.title('Vlaga od 03.02. do 07.02.')
+ts_vlaga.plot(grid=True)
 
 #------------------------------PRIMJENA MODELA---------------------------------
-#Omjeri podataka
-#70% podataka za treniranje
-train = round(len(df)*0.7,0)
-train_int = int(train)
-#30% podataka za testiranje
-test = round(len(df)*0.3,0)
-test_int = int(test)
-
-#Koristenje CO2 stupca
-co2 = df['CO2']
-#Koristenje stupca sa vlagom
-vlaga = df['Vlaga']
-
-#Podjela CO2 podataka u podatke za treniranje/testiranje
-co2_x_train = co2[:train_int]
-co2_x_test = co2[test_int*(-1):]
-
-#Podjela CO2 podataka u podatke za treniranje/testiranje
-co2_y_train = vlaga[:train_int]
-co2_y_test = vlaga[test_int*(-1):]
+#Podjela ulazno/izlaznih podataka na treniranje i testiranje
+#Omjer -> Train:Test = 70:30
+(X_train, X_test, y_train, y_test) = cv.train_test_split(vlaga, co2, test_size=.3, random_state=0)
 
 #-----------------------------Linearna regresija-------------------------------
-
+print("-------------------------------LINEARNA REGRESIJA----------------------------")
 #Koristeni regresijski model
 regr = linear_model.LinearRegression()
 
 #Preoblikovanje elemenata kako bi sklearn mogao koristiti podatke (za X i Y)
-co2x_train = co2_x_train.reshape((321,1))
-co2y_train = co2_y_train.reshape((321,1))
+X_train = X_train.reshape((len(X_train),1))
+X_test = X_test.reshape((len(X_test),1))
 
-co2x_test = co2_x_test.reshape((137,1))
-co2y_test = co2_y_test.reshape((137,1))
+y_train = y_train.reshape((len(y_train),1))
+y_test = y_test.reshape((len(y_test),1))
 
 #Fitanje regresijskog modela
-regr.fit(co2x_train, co2y_train)
+regr.fit(X_train, y_train)
 
 #Plotanje rezultata dobiveni pomocu linearne regresije
-plt.figure(figsize=(8,6), dpi=100)
-plt.scatter(co2x_test, co2y_test, color='orange')
-plt.plot(co2x_test, regr.predict(co2x_test), color='blue', linewidth=2)
+plt.figure(figsize=(10,10))
+plt.plot(X_test, regr.predict(X_test), color='blue', linewidth=2)
+plt.scatter(X_test, y_test, color='orange')
+
 plt.title("Linearna regresija (CO2 i Vlaga)")
 plt.xticks(())
 plt.yticks(())
@@ -130,61 +124,104 @@ plt.show()
 
 #Ispis podataka modela
 print('Koeficijenti: {}'.format(regr.coef_))
-print('Kvadratna pogreska: {}'.format(np.mean((regr.predict(co2x_test)-co2y_test)**2)))
-print('Varijanca: {}'.format(regr.score(co2x_test, co2y_test)))
+print('Kvadratna pogreska: {}'.format(np.mean((regr.predict(X_test)-y_test)**2)))
+print('Varijanca: {}'.format(regr.score(X_test, y_test)))
+
 
 #-----------------------------Logisticka Regresija-----------------------------
+print("-------------------------------LOGISTICKA REGRESIJA----------------------------")
+#Primjena modela za ucenje
+lr = linear_model.LogisticRegression()
 
-#Korištena metoda Logističke Regresije
-clf = linear_model.LogisticRegressionCV()
-#Preoblikovanje u matricni oblik posto radi s takvim oblikom podataka
-co2 = co2.reset_index().as_matrix()
-#co2 = df[['Temperatura','CO2']].as_matrix()
-
-
-##Podjela CO2 podataka u podatke za treniranje/testiranje
-#co2_x_train = co2[:train_int]
-#co2_x_test = co2[test_int*(-1):]
-#
-##Podjela CO2 podataka u podatke za treniranje/testiranje
-#prisutnost_y_train = prisutnost[:train_int]
-#prisutnost_y_test = prisutnost[test_int*(-1):]
-#prisutnost = prisutnost.reshape(458,1).ravel()
-
-clf.fit(co2,prisutnost)
-#Fit - train podaci, predict - test
-#Finoca crtanje granice odluke
-h = 1
-
-#Podaci sluze pregledniji prikaz podataka na grafu
-#Odabir min i max vrijednosti od svih elemenata iz 0. stupca
-x_min, x_max = co2[:, 0].min()-.5, co2[:, 0].max()+.5
-
-#Odabir min i max vrijednosti od svih elemenata iz 1. stupca
-y_min, y_max = co2[:, 1].min()-.5, co2[:, 1].max()+.5
+#Podjela podataka na treniranje i testiranje pri cemu je X = co2, a Y = prisutnost
+#Omjer podjele je treniranje:testiranje=7:3
+(X_train, X_test, y_train, y_test) = cv.train_test_split(co2, prisutnost, test_size=.3, random_state=0)
+print("------------------------------CO2/PRISUTNOST---------------------------------------")
+print ('Distribucija po klasama testnog skupa:')
+print y_test.value_counts()
+print ("Postotak jedinica (prisutan): {:2f}%".format(y_test.mean()*100))
+print ("Postotak nula (nije prisutan): {:2f}%".format(100-y_test.mean()*100))
+X_train = X_train.reshape((len(X_train),1))
+y_train = y_train.reshape((len(y_train),1)).ravel()
+lr.fit(X_train, y_train)
+print ('Preciznost na podacima za treniranje: {:2f} %'.format(lr.score(X_train,y_train)*100))
 
 
-#Izvlacenje koordinata x_min, y_min, x_max i y_max u matricnom obliku
-#h => velicina jednog koraka
-xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
-Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+X_test = X_test.reshape(len(X_test),1)
+y_test = y_test.reshape(len(y_test),1)
+y_predikcija = lr.predict(X_test)
 
-# Put the result into a color plot
-Z = Z.reshape(xx.shape)
-plt.figure(1, figsize=(8,6), dpi=100)
-plt.pcolormesh(xx, yy, Z, cmap=plt.cm.Paired)
+print ('Preciznost na izlaznim predikcijskim i testnim podacima: {:2f} %'
+.format(metrics.accuracy_score(y_test,y_predikcija)*100))
 
-# Plot also the training points
-plt.scatter(co2[:, 0], co2[:, 1], c=prisutnost, edgecolors='k', cmap=plt.cm.Paired)
-plt.xlabel('Index')
-plt.ylabel('CO2')
-plt.title("Logisticka regresija")
-#Granice na grafu za X i Y
-plt.xlim(xx.min(), xx.max())
-plt.ylim(yy.min(), yy.max())
+#Ispis prvih 25 vrijednosti True i Predictet klasa
+print ("Usporedba pravih i predvidjenih vrijednosti")
+print ('True:',y_test[:23].reshape(1,23))
+print ('Pred:',y_predikcija[:23])
 
-plt.xticks(())
-plt.yticks(())
+vjerojatnosti = lr.predict_proba(X_test)
+print ()
+labels = ['Nije prisutan', 'Prisutan']
+cm = metrics.confusion_matrix(y_test, y_predikcija)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+cax = ax.matshow(cm)
+ax.set_xticklabels([''] + labels)
+ax.set_yticklabels([''] + labels,rotation='vertical')
+for i in range (len(cm)):
+    for j in range (len(cm)):
+        ax.annotate(str(cm[j][i]), xy = (i,j),fontsize=25)
 
+plt.colorbar(cax)
+plt.title("Konfuzijska matrica (CO2|Prisutnost)")
+plt.xlabel('Predicted')
+plt.ylabel('True')
 plt.show()
-# 
+
+print ("Tocnost klasifikacije: {} %".format(metrics.accuracy_score(y_test, y_predikcija)*100))
+
+#--------------------------------------Temperatura/CO2-----------------------------------
+
+print("------------------------------TEMPERATURA/PRISUTNOST-----------------------------------")
+(X_train, X_test, y_train, y_test) = cv.train_test_split(temperatura, prisutnost, test_size=.3, random_state=0)
+
+X_train = X_train.reshape((len(X_train),1))
+y_train = y_train.reshape((len(y_train),1)).ravel()
+lr.fit(X_train, y_train)
+print ('Preciznost na podacima za treniranje: {:2f} %'.format(lr.score(X_train,y_train)*100))
+
+
+X_test = X_test.reshape(len(X_test),1)
+y_test = y_test.reshape(len(y_test),1)
+y_predikcija = lr.predict(X_test)
+
+print ('Preciznost na izlaznim predikcijskim i testnim podacima: {:2f} %'
+.format(metrics.accuracy_score(y_test,y_predikcija)*100))
+
+#Ispis prvih 25 vrijednosti True i Predictet klasa
+print ("Usporedba pravih i predvidjenih vrijednosti")
+print ('True:',y_test[:23].reshape(1,23))
+print ('Pred:',y_predikcija[:23])
+
+vjerojatnosti = lr.predict_proba(X_test)
+labels = ['Nije prisutan', 'Prisutan']
+cm = metrics.confusion_matrix(y_test, y_predikcija)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+cax = ax.matshow(cm)
+ax.set_xticklabels([''] + labels)
+ax.set_yticklabels([''] + labels,rotation='vertical')
+for i in range (len(cm)):
+    for j in range (len(cm)):
+        ax.annotate(str(cm[j][i]), xy = (i,j),fontsize=25)
+
+plt.colorbar(cax)
+plt.title("Konfuzijska matrica (Temperatura|Prisutnost)")
+plt.xlabel('Predicted')
+plt.ylabel('True')
+plt.show()
+
+print ("Tocnost klasifikacije: {} %".format(metrics.accuracy_score(y_test, y_predikcija)*100))
+
+
+
